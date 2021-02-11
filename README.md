cdaa4005e227733e8d2fe398324a0c627fb00414
prschoenfelder <phillip.schoenfelder@rub.de>
2021-02-11 14:22:15 +0100

    fixed setup file and filled README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # utils
+
+This is a collection of util functions to make life easier.
+
+Install using pip by
+
+    pip install git+https://github.com/prschoenfelder/utils.git
--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='prschoenfelder-utils',
+    version='0.0.1',
     packages=find_packages(),
     url='https://github.com/prschoenfelder/utils',
     description='This contains a collection of util functions.',
     long_description=open('README.md').read(),
-    install_requires=[
-        "requests==3.5.0"
-        ],
     include_package_data=True,
 ) 
