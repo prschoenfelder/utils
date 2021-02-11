076d6292c60fd987af2b8619fa85bb6fda2fa74c
prschoenfelder <phillip.schoenfelder@rub.de>
2021-02-11 14:26:44 +0100

    changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='prschoenfelder-utils',
+    name='prsutils',
     version='0.0.1',
     packages=find_packages(),
     url='https://github.com/prschoenfelder/utils',
